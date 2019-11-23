037d6a57a21c5b291399d08d0f33c95e2e4c7d0a
David <42595464+l2642235863@users.noreply.github.com>
2019-11-23 14:51:51 +0800

    Add files via upload

--- config.sh
+++ config.sh
@@ -2,15 +2,40 @@ AUTOMOUNT=true
 PROPFILE=false
 POSTFSDATA=ture
 LATESTARTSERVICE=true
-version="1.9.12 Extreme Beta"
+# 模块版本号
+version="1.9.13 Extreme Beta"
+# 模块精简列表更新日期
+update_date="11.23"
+# Zram调整配置(默认启用)
+enable_zram="1"
+# 预读调整(默认启用)
+enable_prefetch="1"
 print_modname() {
   ui_print "*******************************"
   ui_print "          MIUI精简计划   "
   ui_print "        Made by 雄氏老方"
   ui_print "            Extreme "
   ui_print "*******************************"
-  echo -e "\n" >>$INSTALLER/module.prop
-  echo -n "version=$version" >>$INSTALLER/module.prop
+}
+# 自定义配置
+costom_setttings(){
+# 预读大小调整(128kb)
+  if [ $enable_prefetch = "1" ] ; then
+	 echo -e "sleep 9 \nchmod 775 /sys/block/mmcblk0/queue/read_ahead_kb \nchmod 775 /sys/block/mmcblk0/queue/iostats \nwhile true; do \necho '128' > /sys/block/mmcblk0/queue/read_ahead_kb \necho '0' > /sys/block/mmcblk0/queue/iostats \necho '0' > /sys/block/sda/queue/iostats \necho '128' > /sys/block/sda/queue/read_ahead_kb \ndone" >> $INSTALLER/common/service.sh
+  else
+     ui_print ""
+  fi
+# 写入更新日期
+  echo -n "11.23" >>$INSTALLER/module.prop
+
+# Zram调整配置
+  if [ $enable_zram = "0" ] ; then
+    rm -f $INSTALLER/system/etc/mcd_default.conf
+  else
+    echo -n "(已启用Zram调整)" >> $INSTALLER/module.prop
+  fi
+# 写入版本号
+  echo -e "\nversion=$version" >>$INSTALLER/module.prop
 }
 REPLACE="
 /system/app/systemAdSolution
@@ -60,7 +85,6 @@ REPLACE="
 /system/priv-app/MiRcs
 /system/priv-app/MiGameCenterSDKService
 /system/app/TranslationService
-/system/priv-app/MiuiAod
 /system/priv-app/MiuiFreeformService
 /system/priv-app/dpmserviceapp
 /system/priv-app/EmergencyInfo
