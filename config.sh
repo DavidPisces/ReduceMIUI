2e8df56a179c9fe5fdf34cc171e7b42d50af73f1
David <42595464+l2642235863@users.noreply.github.com>
2019-11-30 09:26:48 +0800

    Add files via upload

--- config.sh
+++ config.sh
@@ -3,13 +3,15 @@ PROPFILE=false
 POSTFSDATA=ture
 LATESTARTSERVICE=true
 # 模块版本号
-version="1.9.13 Extreme Beta"
+version="2.0.1 Extreme Beta"
 # 模块精简列表更新日期
-update_date="11.23"
-# Zram调整配置(默认启用)
-enable_zram="1"
+update_date="11.30"
+# Zram调整配置(默认关闭)
+enable_zram="0"
 # 预读调整(默认启用)
 enable_prefetch="1"
+# 自选模式(使用shell冻结)
+enable_mode2="1"
 print_modname() {
   ui_print "*******************************"
   ui_print "          MIUI精简计划   "
@@ -17,16 +19,69 @@ print_modname() {
   ui_print "            Extreme "
   ui_print "*******************************"
 }
+costom_delete(){
+# 写入service.sh，使用循环执行，以防抽风
+ echo -e "\nwhile true; do" >> $INSTALLER/common/service.sh
+# 自选模式
+if [ $enable_mode2 = "1" ]; then
+	ui_print "- 请按音量键选择是否冻结[浏览器] -"
+    ui_print "   [音量+]：冻结"
+    ui_print "   [音量-]：放弃"
+	# 浏览器
+    if $FUNCTION; then
+	    echo -e "\npm disable com.android.browser" >>$INSTALLER/common/service.sh
+	    num=1
+	    ui_print "   [浏览器]冻结成功。"
+    else
+	    ui_print "   [浏览器]未冻结。"
+    fi
+	#内容中心
+	ui_print "- 请按音量键选择是否冻结[内容中心] -"
+    ui_print "   [音量+]：冻结"
+    ui_print "   [音量-]：放弃"
+    if $FUNCTION; then
+	    echo -e "\npm disable com.miui.newhome" >>$INSTALLER/common/service.sh
+	    num=1
+	    ui_print "   [内容中心]冻结成功。"
+    else
+	    ui_print "   [内容中心]未冻结。"
+    fi
+	#小米音乐
+	ui_print "- 请按音量键选择是否冻结[小米音乐] -"
+    ui_print "   [音量+]：冻结"
+    ui_print "   [音量-]：放弃"
+	if $FUNCTION; then
+	    echo -e "\npm disable com.miui.player" >>$INSTALLER/common/service.sh
+	    num=1
+	    ui_print "   [小米音乐]冻结成功。"
+    else
+	    ui_print "   [小米音乐]未冻结。"
+    fi
+	#小米视频
+	ui_print "- 请按音量键选择是否冻结[浏小米视频] -"
+    ui_print "   [音量+]：冻结"
+    ui_print "   [音量-]：放弃"
+	if $FUNCTION; then
+	    echo -e "\npm disable com.miui.video" >>$INSTALLER/common/service.sh
+	    num=1
+	    ui_print "   [小米视频]冻结成功。"
+    else
+	    ui_print "   [小米视频]未冻结。"
+    fi
+else
+ ui_print "- 没有使用自选冻结模式，跳过此部分"
+fi
+}
 # 自定义配置
 costom_setttings(){
 # 预读大小调整(128kb)
   if [ $enable_prefetch = "1" ] ; then
-	 echo -e "sleep 9 \nchmod 775 /sys/block/mmcblk0/queue/read_ahead_kb \nchmod 775 /sys/block/mmcblk0/queue/iostats \nwhile true; do \necho '128' > /sys/block/mmcblk0/queue/read_ahead_kb \necho '0' > /sys/block/mmcblk0/queue/iostats \necho '0' > /sys/block/sda/queue/iostats \necho '128' > /sys/block/sda/queue/read_ahead_kb \ndone" >> $INSTALLER/common/service.sh
+	 echo -e "sleep 7 \nchmod 775 /sys/block/mmcblk0/queue/read_ahead_kb \nchmod 775 /sys/block/mmcblk0/queue/iostats \necho '128' > /sys/block/mmcblk0/queue/read_ahead_kb \necho '0' > /sys/block/mmcblk0/queue/iostats \necho '0' > /sys/block/sda/queue/iostats \necho '128' > /sys/block/sda/queue/read_ahead_kb" >> $INSTALLER/common/service.sh
   else
      ui_print ""
   fi
 # 写入更新日期
-  echo -n "11.23" >>$INSTALLER/module.prop
+  echo -n "$update_date" >>$INSTALLER/module.prop
 
 # Zram调整配置
   if [ $enable_zram = "0" ] ; then
@@ -34,8 +89,9 @@ costom_setttings(){
   else
     echo -n "(已启用Zram调整)" >> $INSTALLER/module.prop
   fi
-# 写入版本号
+# 写入版本号和配置
   echo -e "\nversion=$version" >>$INSTALLER/module.prop
+  echo -e "\ndone" >> $INSTALLER/common/service.sh
 }
 REPLACE="
 /system/app/systemAdSolution
@@ -61,7 +117,7 @@ REPLACE="
 /system/app/AutoRegistration
 /system/app/KSICibaEngine
 /system/app/PrintSpooler
-/syatem/app/PrintRecommendationService
+/system/app/PrintRecommendationService
 /system/app/SeempService
 /system/app/com.miui.qr
 /system/app/Traceur
@@ -88,7 +144,8 @@ REPLACE="
 /system/priv-app/MiuiFreeformService
 /system/priv-app/dpmserviceapp
 /system/priv-app/EmergencyInfo
-/system/priv-app/ExternalStorageProvider
+/system/data-app
+/system/vendor/data-app
 "
 set_permissions() {
   set_perm_recursive  $MODPATH  0  0  0755  0644
