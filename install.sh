28e8de9f7bad961236e166448fd2a9ff220b8f85
泠洛 <42595464+DavidPisces@users.noreply.github.com>
2021-07-18 15:43:10 +0800

    Update ReduceMIUI 2.3 beta
    Support clean MIUI wifi logs.
    Add uninstall script.
    Add some R apps.
    Fix flash problems.

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 # ReduceMIUI 精简计划 配置文件
 # Made by @雄氏老方
-# 跳过挂载
-SKIPMOUNT=false
 # 如果您需要加载system.prop，请将其设置为true
 PROPFILE=true
 # 如果您需要post-fs-data脚本（post-fs-data.sh），请将其设置为true
@@ -9,16 +7,18 @@ POSTFSDATA=true
 # 如果您需要late_start服务脚本（service.sh），请将其设置为true
 LATESTARTSERVICE=true
 # 模块版本号
-version="2.20-PreviewDemo"
+version="2.3 Beta"
 # 模块精简列表更新日期
-update_date="20.5.31"
+update_date="21.7.18"
 # Zram调整配置(默认关闭)
 enable_zram=false
 # SDK判断
-sdk=`grep_prop ro.build.version.sdk`
+sdk=$(grep_prop ro.build.version.sdk)
 # 所要求最小版本
 min_sdk=29
 Enable_determination=false
+# chattr强制阻止log写入(默认关闭，需要busybox)
+is_clean_logs=true
 # 精简列表
 REPLACE="
 /system/app/systemAdSolution
@@ -35,7 +35,6 @@ REPLACE="
 /system/app/mid_test
 /system/app/MiuiVpnSdkManager
 /system/app/BookmarkProvider
-/system/app/CarrierDefaultApp
 /system/app/FidoAuthen
 /system/app/FidoClient
 /system/app/FidoCryptoService
@@ -80,45 +79,58 @@ REPLACE="
 /system/product/priv-app/seccamservice
 /system/data-app
 /system/vendor/data-app
+/system/system_ext/app/PerformanceMode/
+/system/system_ext/app/xdivert/
+/system/system_ext/app/QdcmFF/
+/system/system_ext/app/QColor/
+/system/system_ext/priv-app/EmergencyInfo/
 /vendor/app/GFManager/
 /vendor/app/GFTest/
 "
 sdk_determination() {
-  if [ $sdk -ge $min_sdk ] ; then
+  if [ $sdk -ge $min_sdk ]; then
     ui_print "- 当前SDK为：$sdk"
   else
     abort "- 当前SDK为：$sdk，不符合要求最低SDK：$min_sdk"
-	ui_print "- ! 安装终止"
+    ui_print "- ! 安装终止"
   fi
 }
-costom_setttings(){
-# 版本判断启用配置
-  if [ $Enable_determination = true ] ; then
+costom_setttings() {
+  # 版本判断启用配置
+  if [ $Enable_determination = true ]; then
     sdk_determination
   fi
-# 写入更新日期
-  echo -n "$update_date" >> $TMPDIR/module.prop
-# Zram调整配置
-  if [ $enable_zram = false ] ; then
+  # 写入更新日期
+  echo -n "$update_date" >>$TMPDIR/module.prop
+  # Zram调整配置
+  if [ $enable_zram = false ]; then
     rm -f /data/adb/modules_update/Reducemiui/system/etc/mcd_default.conf
-	ui_print "- Zram配置未启用，若使用配置文件请修改模块配置文件install.sh"
+    ui_print "- Zram配置未启用，若使用配置文件请修改模块配置文件install.sh"
   else
-    echo -n "(已启用Zram调整)" >> $TMPDIR/module.prop
-	ui_print "- Zram配置已启用，若关闭配置文件请修改模块配置文件install.sh"
+    echo -n "(已启用Zram调整)" >>$TMPDIR/module.prop
+    ui_print "- Zram配置已启用，若关闭配置文件请修改模块配置文件install.sh"
   fi
-# 写入版本号
-  echo -e "\nversion=$version" >> $TMPDIR/module.prop
-}
-auto_uninstall_AD_apps(){
-  pm uninstall com.miui.analytics
-  ui_print "! 已清除Analytics"
-  echo -e "sleep 10\npm uninstall com.miui.analytics" >> $TMPDIR/common/service.sh
-# 有待添加
+  # 写入版本号
+  echo -e "\nversion=$version" >>$TMPDIR/module.prop
 }
 
-on_install(){
+on_install() {
   ui_print "- 提取模块文件"
   unzip -o "$ZIPFILE" 'system/*' -d $MODPATH >&2
+}
+
+clean_wifi_logs() {
+  if [ $is_clean_logs = true ]; then
+    ui_print "-! 正在清除MIUI WiFi log"
+    rm -rf /data/vendor/wlan_logs/*
+    ui_print "-! 正在锁定log文件阻止读写"
+    chattr +i /data/vendor/wlan_logs/
+    ui_print "-! 已锁定WiFi日志文件"
+  fi
+    touch /storage/emulated/0/MIUI/uninstall_ReduceMIUI.sh
+    echo -e "#! /sbin/sh \nchattr -i /data/vendor/wlan_logs/ \nrm -rf /data/adb/modules/Reducemiui/ \necho '-! 卸载完成，请重启手机' " > /storage/emulated/0/MIUI/uninstall_ReduceMIUI.sh
+    ui_print "-! 卸载脚本创建完成"
+    ui_print "- 卸载时请使用/storage/emulated/0/MIUI/uninstall_ReduceMIUI.sh卸载模块"
 }
 
 on_install
@@ -128,5 +140,4 @@ ui_print "  Reduce MIUI Project"
 ui_print "  "
 ui_print "  "
 costom_setttings
-#banlist
-auto_uninstall_AD_apps
+clean_wifi_logs
