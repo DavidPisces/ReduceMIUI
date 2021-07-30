e990dbc76d244ace82e04a5a966d95627ae6731f
赵悦男 <zhaoyuenan@zhaoyuenan.xxx>
2021-07-31 00:16:43 +0800

    [ReduceMIUI][SERVICE]common: Add basic optimization to the service
    * Disable I/O statistics for the main data partition
    * Disable I/O statistics for secondary data partitions
    * Disable binder job log output
    * Disable binder_alloc work log output
    * Change the disk read-ahead to 128k to improve I/O performance (sda)
    * Change the disk read-ahead to 128k to improve I/O performance (sde)
      ** Maybe you can add dm-5, because the user uses the official change,
         do not add it temporarily **
    * Adjust some miscellaneous project target changes
    
    [Gerrit-stic-bq054-bd: 赵悦男改动 未添加描述]
    [Solution: 赵悦男改动 未添加描述]
    [Android Verson: 赵悦男改动 未添加描述]
    [changes test: 赵悦男正常启动 未添加描述]
    
    BUG: 70496253
    Signed-Off-By: 赵悦男 <2777968262@qq.com>
    Tested-By: 赵悦男 <2777968262@qq.com>
    Change-Id: I0bdba6791a16601931453a96dda9122ce6efc6c5

--- common/service.sh
+++ common/service.sh
@@ -5,3 +5,38 @@ stop tcpdump
 stop cnss_diag
 rm -rf /data/vendor/charge_logger/
 rm -rf /data/vendor/wlan_logs/
+
+### ADD: 赵悦男添加: MIUI 基础优化 2021-0730-235123 ###
+### ADD: 赵悦男添加标记 TODO: 基于小米10 12.5.3稳定版测试 2021-0730-235129 ###
+### END: 赵悦男添加标记 TODO: 基于小米10 12.5.3稳定版测试 2021-0730-235129 ###
+#### ADD: 赵悦男添加标记 TODO: 禁用磁盘I/O读写统计 2021-0730-235204 ####
+#### ADD: 赵悦男添加标记 TODO: 禁用sda主数据分区读写统计 2021-0730-235209 ####
+echo "0" > /sys/block/sda/queue/iostats
+#### ADD: 赵悦男添加标记 TODO: 禁用sde次要数据分区读写统计 2021-0730-235213 ####
+echo "0" > /sys/block/sde/queue/iostats
+#### END: 赵悦男添加标记 TODO: 禁用磁盘I/O读写统计 2021-0730-235204 ####
+#### ADD: 赵悦男添加标记 TODO: 禁用binder活页夹日志输出 2021-0730-235222 ####
+echo "0" > /sys/module/binder/parameters/debug_mask
+#### END: 赵悦男添加标记 TODO: 禁用binder活页夹日志输出 2021-0730-235222 ####
+#### ADD: 赵悦男添加标记 TODO: 禁用binder活页夹分配器日志输出 2021-0730-235231 ####
+echo "0" > /sys/module/binder_alloc/parameters/debug_mask
+#### END: 赵悦男添加标记 TODO: 禁用binder活页夹分配器日志输出 2021-0730-235231 ####
+#### ADD: 赵悦男添加标记 TODO: 更改磁盘预读为128k sda主分区 2021-0730-235237 ####
+echo "128" > /sys/block/sda/queue/read_ahead_kb
+#### END: 赵悦男添加标记 TODO: 更改磁盘预读为128k sda主分区 2021-0730-235237 ####
+#### ADD: 赵悦男添加标记 TODO: 更改磁盘预读为128k sde次要分区 2021-0730-235240 ####
+echo "128" > /sys/block/sde/queue/read_ahead_kb
+#### END: 赵悦男添加标记 TODO: 更改磁盘预读为128k sde次要分区 2021-0730-235240 ####
+#### ADD: 赵悦男添加标记 TODO: 更改磁盘NR分配块 sda主分区 2021-0730-235245 ####
+echo "36" > /sys/block/sda/queue/nr_requests
+#### END: 赵悦男添加标记 TODO: 更改磁盘NR分配块 sda主分区 2021-0730-235245 ####
+#### ADD: 赵悦男添加标记 TODO: 更改磁盘NR分配块 sde次要分区 2021-0730-235247 ####
+echo "36" > /sys/block/sde/queue/nr_requests
+#### END: 赵悦男添加标记 TODO: 更改磁盘NR分配块 sde次要分区 2021-0730-235247 ####
+#### ADD: 赵悦男添加标记 TODO: 更改虚拟内存page集群数量 2021-0730-235301 ####
+echo "0" > /proc/sys/vm/page-cluster
+#### END: 赵悦男添加标记 TODO: 更改虚拟内存page集群数量 2021-0730-235301 ####
+#### ADD: 赵悦男添加标记 TODO: 更改虚拟内存更新间隔, 减少性能抖动 2021-0730-235312 ####
+echo "20" > /proc/sys/vm/stat_interval
+#### END: 赵悦男添加标记 TODO: 更改虚拟内存更新间隔, 减少性能抖动 2021-0730-235312 ####
+### END: 赵悦男添加: MIUI 基础优化 2021-0730-235123 ###
